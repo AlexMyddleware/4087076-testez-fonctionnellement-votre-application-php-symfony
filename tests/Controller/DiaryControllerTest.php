e071362c2143fc6d163e7c639cbd88a928e6b20b
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-07-06 08:38:40 +0200

    feat: diary controller test

--- tests/Controller/DiaryControllerTest.php
+++ tests/Controller/DiaryControllerTest.php
@@ -4,14 +4,35 @@ namespace App\Tests\Controller;
 
 use App\Entity\User;
 use App\Services\Diary;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DiaryControllerTest extends WebTestCase
 {
-    public function testCaloriesStatusAction()
-    {
-        $client = static::createClient();
-        // assert true = true
-        $this->assertTrue(true);
-    }
+    private $client;
+    private $userRepository;
+    private $user;
+    private $urlGenerator;
+
+    public function setUp() : void
+
+     {
+        $this->client = static::createClient();
+
+        $this->userRepository = $this->client->getContainer()->get('doctrine.orm.entity_manager')->getRepository(User::class);
+        $this->user = $this->userRepository->findOneByEmail('richard11@dbmail.com');
+
+        $this->urlGenerator = $this->client->getContainer()->get('router.default');
+
+        $this->client->loginUser($this->user);
+   }
+
+    public function testHomepageIsUp()
+
+   {
+        $this->client->request(Request::METHOD_GET, $this->urlGenerator->generate('homepage'));
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+   }
 }
