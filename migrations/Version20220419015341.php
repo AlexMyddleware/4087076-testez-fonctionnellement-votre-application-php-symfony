2824e47da5392bd796726ec52c0cc0f52e135431
AlexMyddleware <106162060+AlexMyddleware@users.noreply.github.com>
2023-07-06 10:42:14 +0200

    Update Version20220419015341.php

--- migrations/Version20220419015341.php
+++ migrations/Version20220419015341.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20220419015341 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,26 +16,23 @@ final class Version20220419015341 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74');
-        $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, username, fullname, email, avatar_url, profile_html_url, github_id, password FROM user');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__user SELECT id, username, fullname, email, avatar_url, profile_html_url, github_id, password FROM user');
         $this->addSql('DROP TABLE user');
-        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(255) NOT NULL, fullname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, avatar_url VARCHAR(255) NOT NULL, profile_html_url VARCHAR(255) NOT NULL, github_id VARCHAR(255) DEFAULT NULL, password VARCHAR(255) DEFAULT \'0\' NOT NULL, roles CLOB DEFAULT NULL
-        )');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, fullname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, avatar_url VARCHAR(255) NOT NULL, profile_html_url VARCHAR(255) NOT NULL, github_id VARCHAR(255) DEFAULT NULL, password VARCHAR(255) DEFAULT \'0\' NOT NULL, roles TEXT DEFAULT NULL)');
         $this->addSql('INSERT INTO user (id, username, fullname, email, avatar_url, profile_html_url, github_id, password) SELECT id, username, fullname, email, avatar_url, profile_html_url, github_id, password FROM __temp__user');
-        $this->addSql('DROP TABLE __temp__user');
+        $this->addSql('DROP TEMPORARY TABLE __temp__user');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74');
-        $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, username, fullname, email, avatar_url, profile_html_url, github_id, password FROM user');
+        $this->addSql('DROP INDEX UNIQ_8D93D649E7927C74 ON user');
+        $this->addSql('CREATE TEMPORARY TABLE __temp__user SELECT id, username, fullname, email, avatar_url, profile_html_url, github_id, password FROM user');
         $this->addSql('DROP TABLE user');
-        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(255) NOT NULL, fullname VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT NULL, avatar_url VARCHAR(255) NOT NULL, profile_html_url VARCHAR(255) NOT NULL, github_id VARCHAR(255) DEFAULT NULL, password VARCHAR(255) DEFAULT \'0\' NOT NULL)');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255) NOT NULL, fullname VARCHAR(255) NOT NULL, email VARCHAR(255) DEFAULT NULL, avatar_url VARCHAR(255) NOT NULL, profile_html_url VARCHAR(255) NOT NULL, github_id VARCHAR(255) DEFAULT NULL, password VARCHAR(255) DEFAULT \'0\' NOT NULL)');
         $this->addSql('INSERT INTO user (id, username, fullname, email, avatar_url, profile_html_url, github_id, password) SELECT id, username, fullname, email, avatar_url, profile_html_url, github_id, password FROM __temp__user');
-        $this->addSql('DROP TABLE __temp__user');
+        $this->addSql('DROP TEMPORARY TABLE __temp__user');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
     }
 }
